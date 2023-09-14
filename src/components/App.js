ba8d87e0fcce4015c8d2b063f0bd9cf870cf5d9e
MaggieWesolowska <maggiewes07@gmail.com>
2023-09-14 22:08:38 +0200

    added fetch and logic for components

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,76 @@ import Form from './Form';
 import Result from './Result';
 import './App.css';
 
+const API_KEY = '378d8782aac3425bf266992253fc2a56';
+
+// 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=378d8782aac3425bf266992253fc2a56&units=metric'
+
 class App extends Component {
   state = {
     value: '',
+    date: '',
+    city: '',
+    sunrise: '',
+    sunset: '',
+    temp: '',
+    pressure: '',
+    wind: '',
+    error: false,
   };
 
   handleInputChange = ev => {
-    ev.preventDefault();
     this.setState({
       value: ev.target.value,
     });
   };
 
+  handleCitySubmit = ev => {
+    ev.preventDefault();
+
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${API_KEY}&units=metric`;
+
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          return response;
+        }
+        throw Error(
+          'Sorry, something went wrong.. Try again.'
+        );
+      })
+      .then(response => response.json())
+      .then(result => {
+        const date = new Date().toLocaleString();
+        this.setState({
+          date,
+          city: this.state.value,
+          sunrise: result.sys.sunrise,
+          sunset: result.sys.sunset,
+          temp: result.main.temp,
+          pressure: result.main.pressure,
+          wind: result.wind.speed,
+          error: false,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          error: true,
+          city: this.state.value,
+        });
+      });
+  };
+
   render() {
-    const { value } = this.state;
+    const { value, error } = this.state;
     return (
       <div className='App-header'>
         <Form
           value={value}
           change={this.handleInputChange}
+          submit={this.handleCitySubmit}
         />
-        <Result />
+        <Result weather={this.state} error={error} />
       </div>
     );
   }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
-const Result = () => {
-  return <div>Weather</div>;
+const Result = props => {
+  const {
+    city,
+    sunrise,
+    sunset,
+    temp,
+    pressure,
+    wind,
+    error,
+  } = props.weather;
+
+  let content = null;
+
+  if (!error && city) {
+    content = (
+      <div>
+        <h1>Weather for: {city}</h1>
+        <div>Temperature: {temp}</div>
+        <div>Sunrise at: {sunrise}</div>
+        <div>Sunset at: {sunset}</div>
+        <div>Pressure: {pressure}</div>
+        <div>Wind: {wind}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className='result'>
+      {error ? `City ${city} not found` : content}
+    </div>
+  );
 };
 
 export default Result;
